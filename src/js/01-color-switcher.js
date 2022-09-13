e625c5b29e61256bacea79e6676badda63b4624e
elenashy <97391479+elenashy@users.noreply.github.com>
2022-09-14 01:23:17 +0300

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+}
 
+refs.btnStart.addEventListener('click', onBtnStartClick)
+refs.btnStop.addEventListener('click', onBtnStopClick)
+
+let intervalId = null;
+
+function onBtnStartClick() {
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+
+    return intervalId = setInterval(() => {
+        function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
+    document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+
+}
+
+function onBtnStopClick() {
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+
+    clearInterval(intervalId)
+}
