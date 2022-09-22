3c3dd2f70f0ea47d07374a5f693a9a882ad4e20e
elenashy <97391479+elenashy@users.noreply.github.com>
2022-09-23 00:53:33 +0300

    start 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form'),
+  buttonSubmit: document.querySelector('button'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit)
+const { delay, step, amount } = refs.form.elements;
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }, delay)
+  })
+};
+  
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  let delayValue = Number(delay.value);
+  const amountValue = Number(amount.value);
+
+  for (let i = 1; i < amountValue; i += 1){
+    createPromise(i, delayValue).then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delayValue += Number(step.value);
+};
+  };
+  
+
+
+
